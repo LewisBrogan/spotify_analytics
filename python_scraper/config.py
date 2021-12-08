f8fabb4b1275e59fec6c85f7e22dcef1948d9bb9
Davis Ansons <17002710@uhi.ac.uk>
2021-12-08 16:49:27 +0000

    created config file for scraper and imported config into main

---
+++ python_scraper/config.py
@@ -0,0 +1,4 @@
+USERNAME='spotify username'
+CLIENT_ID='client id'
+CLIENT_SECRET='client secret'
+REDIRECT_URI='http://localhost:8888/'
--- python_scraper/spotify_scraper.py
+++
@@ -1,4 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-from time import sleep
-from datetime import date, timedelta
