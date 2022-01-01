54b1b9f402139f2740dbce2157596828214732d6
iLewisBrogan <ilewisbrogan@gmail.com>
2022-01-01 17:43:20 +0000

    cleaned up the project one last time, staged stg_song_plays, added tests, dbt runs and no errors on the tests

---
+++ models/1_staging/stg_song_plays.sql
@@ -0,0 +1,33 @@
+{%- set columns = 
+['songplays_id',
+'track_id',
+'track_name',
+'track_duration',
+'track_is_explicit',
+'track_popularity',
+'track_played_at',
+'album_id',
+'album_name',
+'album_release_year',
+'album_type',
+'artist_name',
+'artist_name_others',
+'artist_id',
+'artist_id_others',
+'track_danceability'] -%}
+
+/*
+clean the Jinja code so there is no unnecessary whitespace in the compiled sql
+*/
+with song_plays as (
+select
+{%- for col in columns -%}
+  {% if loop.first %}  {{ col }}
+    {% else %}, {{ col }}
+  {% endif %}
+{%- endfor -%}
+from {{ ref('songplays') }}
+)
+select
+  *
+from song_plays
